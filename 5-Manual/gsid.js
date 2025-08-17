7f0456de42c4f4ea43b268c8d6f503c2d0c20f97
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2025-08-18 00:52:57 +0300

    Sync 5th with node.js implementation

--- 5-Manual/gsid.js
+++ 5-Manual/gsid.js
@@ -9,19 +9,20 @@ const CHARS = DIGITS + LOWER + UPPER + '-_';
 const CHARS_LENGTH = CHARS.length;
 
 const POSSIBLE = new Uint8Array(CHARS_LENGTH);
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < CHARS_LENGTH; i++) {
   POSSIBLE[i] = CHARS.charCodeAt(i);
 }
 
 const DEFAULT_LENGTH = 24;
 const BUF_SIZE = DEFAULT_LENGTH * 4096;
+const MAX_LENGTH = 256;
 
-const result = Buffer.allocUnsafe(DEFAULT_LENGTH);
 const randomBuffer = crypto.randomBytes(BUF_SIZE);
+const resultBuffer = Buffer.allocUnsafe(MAX_LENGTH);
 
 let bufferPos = 0;
 
-const generateGSID = (length = DEFAULT_LENGTH) => {
+const generateId = (length = DEFAULT_LENGTH) => {
   if (bufferPos + length > randomBuffer.length) {
     crypto.randomFillSync(randomBuffer);
     bufferPos = 0;
@@ -29,9 +30,9 @@ const generateGSID = (length = DEFAULT_LENGTH) => {
   const start = bufferPos;
   bufferPos += length;
   for (let i = 0; i < length; i++) {
-    result[i] = POSSIBLE[randomBuffer[start + i] & 0x3f];
+    resultBuffer[i] = POSSIBLE[randomBuffer[start + i] & 0x3f];
   }
-  return result.toString('ascii');
+  return resultBuffer.subarray(0, length).toString('ascii');
 };
 
-module.exports = { generateGSID, CHARS };
+module.exports = { generateId, CHARS };
